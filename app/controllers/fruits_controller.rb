a5a41d4f1bf164a7fce01b9ead65bd3eb8fa8b58
ben <bjelinek42@gmail.com>
2022-04-10 13:58:37 -0600

    back create

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -4,4 +4,14 @@ class FruitsController < ApplicationController
     render json: fruit
   end
 
+  def create
+    fruit = Fruit.new(
+      name: params[:name],
+      color: params[:color],
+      price: params[:price]
+    )
+    fruit.save
+    render json: fruit
+  end
+
 end
