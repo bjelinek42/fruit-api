34b4abd2cff4bcb9a88709802f0f702bec931bd6
ben <bjelinek42@gmail.com>
2022-04-10 13:52:25 -0600

    frontend initial

---
+++ fruit-frontend/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () { },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
