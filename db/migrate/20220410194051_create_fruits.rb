c78ce8f6895f2d9312e1ac06a0508c53c8bb4e1d
ben <bjelinek42@gmail.com>
2022-04-10 13:42:03 -0600

    initial

---
+++ db/migrate/20220410194051_create_fruits.rb
@@ -0,0 +1,11 @@
+class CreateFruits < ActiveRecord::Migration[7.0]
+  def change
+    create_table :fruits do |t|
+      t.string :name
+      t.string :color
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
