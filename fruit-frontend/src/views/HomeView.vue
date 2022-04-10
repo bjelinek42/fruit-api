ad1da88959c929d201caf189fa601fac76cefa4d
ben <bjelinek42@gmail.com>
2022-04-10 13:56:04 -0600

    frontend index

--- fruit-frontend/src/views/HomeView.vue
+++ fruit-frontend/src/views/HomeView.vue
@@ -3,17 +3,30 @@ import axios from 'axios'
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to Fruits!",
+      fruits: []
     };
   },
-  created: function () { },
-  methods: {},
+  created: function () {
+    this.indexFruits();
+  },
+  methods: {
+    indexFruits: function () {
+      axios.get("/fruits").then(response => {
+        console.log(response.data);
+        this.fruits = response.data
+      })
+    }
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="fruit in fruits" v-bind:key="fruit">
+      <p>{{ fruit.name }}</p>
+    </div>
   </div>
 </template>
 
